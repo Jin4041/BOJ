23f96ec58267c0b7979db4bfdb708426c92506ff
JIN <126974009+Jin4041@users.noreply.github.com>
2024-04-03 23:39:04 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] solution(int s, int e) {
+        int[] answer = new int[s-e+1];
+        for(int i=0;i<answer.length;i++)answer[i]=s-i;
+        return answer;
+    }
+}
