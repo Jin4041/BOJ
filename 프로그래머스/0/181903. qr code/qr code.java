d8906922046f6a1ffec879ec5e7a8498417e4123
JIN <126974009+Jin4041@users.noreply.github.com>
2024-04-04 13:24:48 +0900

    [level 0] Title: qr code, Time: 11.09 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String s = "";
+        for(int i=0;i<code.length();i++) if(i%q==r) s+=code.charAt(i);
+        return s;
+    }
+}
