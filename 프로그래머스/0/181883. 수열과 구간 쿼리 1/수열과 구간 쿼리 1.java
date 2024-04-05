5f0076e1e9e57ea2c15ce64fd4b871cb2add06db
JIN <126974009+Jin4041@users.noreply.github.com>
2024-04-05 09:35:18 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.77 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] q) {
+        for(int i=0;i<q.length;i++){
+            for(int j=q[i][0];j<=q[i][1];j++) arr[j]++;
+        }
+        return arr;
+    }
+}
