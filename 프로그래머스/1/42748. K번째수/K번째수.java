1552a358d211ed51e54458928f24fad2e8446eea
JIN <126974009+Jin4041@users.noreply.github.com>
2024-05-24 11:13:57 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,12 +1,29 @@
 import java.util.Arrays;
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+    public static int[] solution(int[] array, int[][] commands){
+        int[] answer= new int[commands.length];
         for(int i=0;i<commands.length;i++){
             int[] temp=Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
-            Arrays.sort(temp);
+            bubble(temp);
             answer[i]=temp[commands[i][2]-1];
         }
         return answer;
+    }
+
+     public static void bubble(int[] array){
+        for(int j=0;j<array.length-1;j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array,i, i + 1);
+                }
+
+            }
+        }
+    }
+    
+    public static void swap(int[] array,int a, int b){
+        int temp=array[a];
+        array[a]=array[b];
+        array[b]=temp;
     }
 }
