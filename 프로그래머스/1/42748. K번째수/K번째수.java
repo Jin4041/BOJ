ab5d3f017a5eb5709a5eb120a0784e9f854a392b
JIN <126974009+Jin4041@users.noreply.github.com>
2024-05-24 15:21:44 +0900

    Update K번째수.java

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 class Solution {
     public static int[] solution(int[] array, int[][] commands){
         int[] answer= new int[commands.length];
@@ -20,30 +19,26 @@ class Solution {
     }
 
     public static int partition(int[] array,int start, int end) {
-        int pivot = array[start]; // 피벗을 배열의 첫 번째 요소로 선택
-        int left = start + 1;
-        int right = end;
+        int left=start+1;
+        int right=end;
+        int pivot = array[start];
 
-        while (left <= right) {
-            // 피벗보다 큰 값을 찾음
-            while (left <= end && array[left] <= pivot)
+        while(left<=right){
+            while(left<=end&&array[left]<=pivot)
                 left++;
-            // 피벗보다 작은 값을 찾음
-            while (right >= start && array[right] > pivot)
+            while(right>=start&&array[right]>pivot)
                 right--;
-            // left와 right가 엇갈린 경우 종료
-            if (left > right)
+            if(left>right)
                 break;
-            // left와 right가 아직 엇갈리지 않았다면, 값을 교환
-            int temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
+            swap(array,left, right);
         }
-        // 피벗과 right가 가리키는 값을 교환
-        int temp = array[start];
-        array[start] = array[right];
-        array[right] = temp;
-        // right의 인덱스 반환
+        swap(array,right,start);
         return right;
+    }
+    
+    public static void swap(int[] array,int a, int b){
+        int temp=array[a];
+        array[a]=array[b];
+        array[b]=temp;
     }
 }
